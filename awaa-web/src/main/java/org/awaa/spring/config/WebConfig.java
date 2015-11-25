0d421647727b77a64de606d6ea55f31ba1ba4c15
John Jairo Lopez Arroyave <John Jairo Lopez Arroyave>
2015-11-25 11:01:15 -0500

    Inicialización

---
+++ awaa-web/src/main/java/org/awaa/spring/config/WebConfig.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package org.awaa.spring.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author john.lopez
+ *
+ */
+@Configuration
+public class WebConfig {
+
+}
