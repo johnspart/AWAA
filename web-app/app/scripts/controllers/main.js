0ddcaea82d6155b74b9ecf779c1ebd5dcb3d47f7
John Jairo Lopez Arroyave <John Jairo Lopez Arroyave>
2015-12-03 16:41:16 -0500

    Se agrega el esquema para el proyecto Angular

---
+++ web-app/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name awaa.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the awaa
+ */
+angular.module('awaa')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
