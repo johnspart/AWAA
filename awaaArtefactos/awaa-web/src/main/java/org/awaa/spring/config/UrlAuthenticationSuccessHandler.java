3b279e124fa6e4e21b2e273c1f120edfcdfc72ea
johnspart <best.spart@gmail.com>
2016-01-26 09:21:48 -0500

    Actualización spring security

---
+++ awaaArtefactos/awaa-web/src/main/java/org/awaa/spring/config/UrlAuthenticationSuccessHandler.java
@@ -0,0 +1,75 @@
+/**
+ * 
+ */
+package org.awaa.spring.config;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.awwa.utils.logs.Logs;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.WebAttributes;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+/**
+ * @author john.lopez
+ *
+ */
+public class UrlAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+	private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+			Authentication authentication) throws IOException {
+		handle(request, response, authentication);
+		clearAuthenticationAttributes(request);
+	}
+
+	protected void handle(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
+			throws IOException {
+		String targetUrl = determineTargetUrl(authentication);
+
+		if (response.isCommitted()) {
+			Logs.getInfo().debug("Response has already been committed. Unable to redirect to " + targetUrl);
+			return;
+		}
+
+		//redirectStrategy.sendRedirect(request, response, targetUrl);
+	}
+
+	/**
+	 * Builds the target URL according to the logic defined in the main class
+	 * Javadoc.
+	 */
+	protected String determineTargetUrl(Authentication authentication) {
+		boolean isUser = !authentication.getAuthorities().isEmpty();
+
+		if (isUser)
+			return "/index.html";
+		else
+			throw new IllegalStateException();
+
+	}
+
+	protected void clearAuthenticationAttributes(HttpServletRequest request) {
+		HttpSession session = request.getSession(false);
+		if (session == null) {
+			return;
+		}
+		session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
+	}
+
+	public void setRedirectStrategy(RedirectStrategy redirectStrategy) {
+		this.redirectStrategy = redirectStrategy;
+	}
+
+	protected RedirectStrategy getRedirectStrategy() {
+		return redirectStrategy;
+	}
+}
