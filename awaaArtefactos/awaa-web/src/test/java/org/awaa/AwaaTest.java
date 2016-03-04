072f02075a36f85b4a51950acc0125a5dc1cdec4
john <best.spart@gmail.com>
2016-03-04 15:08:26 -0500

    Pruebas Jenkins

--- awaaArtefactos/awaa-web/src/test/java/org/awaa/AwaaTest.java
+++ awaaArtefactos/awaa-web/src/test/java/org/awaa/AwaaTest.java
@@ -7,13 +7,9 @@ package org.awaa;
  * @author john.lopez
  *
  */
-import org.junit.runner.RunWith;
 
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
-
-@RunWith(Cucumber.class)
-@CucumberOptions(features = { "src/test/java/org/awaa/feature/paginaInicio.feature" })
+//@RunWith(Cucumber.class)
+//@CucumberOptions(features = { "src/test/java/org/awaa/feature/paginaInicio.feature" })
 public class AwaaTest {
 
 }
